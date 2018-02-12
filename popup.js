99ccd0d6b445c26ce03e70e03d8570a86eb0a38f
scache <scal.che@gmail.com>
2018-02-13 00:18:13 +0900

    現在表示しているページのURLをQRコードで表示

--- popup.js
+++ popup.js
@@ -2,5 +2,11 @@
     chrome.tabs.getSelected(null, function (tab) {
         $("#title").text(tab.title)
         $("#url").text(tab.url)
+
+        $("#qrcode").qrcode({
+            text: tab.url,
+            width: 256,
+            height: 256
+        })
     })
 }.call(this))
